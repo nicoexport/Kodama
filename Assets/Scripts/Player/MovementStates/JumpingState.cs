8c28918e0a01765eb2e7354fce3286c34a1036e0
Export_Nico <auersw.nico@gmail.com>
2021-11-04 00:04:57 +0100

    regular jump mit addforce implementiert

--- Assets/Scripts/Player/MovementStates/JumpingState.cs
+++ Assets/Scripts/Player/MovementStates/JumpingState.cs
@@ -17,7 +17,8 @@ public class JumpingState : AirbourneState
     private void Jump(bool canLongJump, float jumpForce)
     {
         character.transform.Translate(Vector2.up * (character.groundCheckRadius + 0.1f));
-        character.rb.velocity = new Vector2(character.rb.velocity.x, jumpForce);
+        var force = new Vector2(0f, jumpForce);
+        character.rb.AddForce(force, ForceMode2D.Impulse);
         if (canLongJump)
         {
             keepJumping = Input.GetButton("Jump");
@@ -56,7 +57,10 @@ public class JumpingState : AirbourneState
         // makes the character keep jumping if the jump button is held
         if (keepJumping && keepJumpingTimer > 0f)
         {
-            character.rb.velocity = new Vector2(character.rb.velocity.x, character.jumpForce);
+
+            var force = new Vector2(0f, character.jumpForce * 3.5f);
+            character.rb.AddForce(force, ForceMode2D.Force);
+
             keepJumpingTimer -= Time.deltaTime;
         }
     }
