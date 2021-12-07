f2d0ad3f4fda89f3d817b8e728819b23e2226925
Export_Nico <auersw.nico@gmail.com>
2021-12-07 21:42:14 +0100

    moving platform added

---
+++ Assets/ObjectMover.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] wayPoints;
+    private int currentWayPointIndex = 0;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private bool movePlayer = true;
+
+
+    private void Start()
+    {
+        currentWayPointIndex = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveObject();
+    }
+
+    private void MoveObject()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPointIndex].position, speed / 100f);
+        if (Vector2.Distance(transform.position, wayPoints[currentWayPointIndex].position) <= 0.1f) currentWayPointIndex += 1;
+        if (currentWayPointIndex >= wayPoints.Length) currentWayPointIndex = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collision Enter");
+        if (movePlayer && col.collider.tag == "Player")
+        {
+            col.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        Debug.Log("Collision Exit");
+        if (col.collider.tag == "Player")
+        {
+            col.collider.transform.SetParent(null);
+        }
+    }
+}
