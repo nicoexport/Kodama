5f3354416130b4568e4d4e589acc33edacd604ac
Export_Nico <auersw.nico@gmail.com>
2021-12-14 15:39:06 +0100

    Object Spawner and Falling Damage Block Prefabs added

--- Assets/DamageZone.cs
+++ Assets/DamageZone.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class DamageZone : MonoBehaviour
 {
     public int damage = 1;
+    [SerializeField]
+    private bool destroyedOnGroundCollision = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player") other.GetComponent<Character>().LifeHandler.TakeDamage(damage);
+        if (other.tag == "Player")
+        {
+            var character = other.GetComponent<Character>();
+            if (character != null) character.LifeHandler.TakeDamage(damage);
+        }
+        if (destroyedOnGroundCollision && other.tag == "Ground")
+        {
+            Destroy(gameObject, 0.1f);
+        }
     }
 }
