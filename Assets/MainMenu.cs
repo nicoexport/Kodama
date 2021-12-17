c83bbe4f27e68ff201cfa8fc26fd48abb3ed9b79
Export_Nico <auersw.nico@gmail.com>
2021-12-17 21:02:29 +0100

    Main menu and Level Select Menu finished

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,9 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private LevelObject defaultLevel;
+
+    [Space(10)]
+    public MenuState state;
+    public enum MenuState { main, level, settings }
 
 
+    [Space(10)]
+    [Header("Canvases")]
+    [SerializeField]
+    private GameObject mainCanvas;
+    [SerializeField]
+    private GameObject levelCanvas;
+    [SerializeField]
+    private GameObject settingsCanvas;
+
+    [Header("Buttons")]
+    [SerializeField]
+    private Button primaryButtonMain;
+    [SerializeField]
+    private Button primaryButtonLevel;
+    [SerializeField]
+    private Button primaryButtonSettings;
+
+
+
+    private void Start()
+    {
+        SwitchMenu("main");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(defaultLevel.scenePath);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchMenu(string menuname)
+    {
+        switch (menuname)
+        {
+            case "main":
+                levelCanvas.SetActive(false);
+                settingsCanvas.SetActive(false);
+                mainCanvas.SetActive(true);
+                primaryButtonMain.Select();
+                state = MenuState.main;
+                break;
+            case "settings":
+                mainCanvas.SetActive(false);
+                levelCanvas.SetActive(false);
+                settingsCanvas.SetActive(true);
+                primaryButtonSettings.Select();
+                state = MenuState.settings;
+                break;
+
+            case "level":
+                mainCanvas.SetActive(false);
+                settingsCanvas.SetActive(false);
+                levelCanvas.SetActive(true);
+                primaryButtonLevel.Select();
+                state = MenuState.level;
+                break;
+
+            default:
+                return;
+        }
+    }
 }
