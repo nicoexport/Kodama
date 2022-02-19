25bc85be2e73a8fa337a015926fd59c9af446e04
Export_Nico <auersw.nico@gmail.com>
2022-02-19 19:11:14 +0100

    Level assets angepasst und WorldObject.cs hinzugefügt

--- Assets/Scripts/World_Level/WorldObject.cs
+++ Assets/Scripts/World_Level/WorldObject.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "WorldObject")]
-public class World : ScriptableObject
+public class WorldObject : ScriptableObject
 {
     public string worldName;
     public int worldIndex;
-    public LevelObject[] levels;
+    public Sprite WorldImage;
+    public List<LevelObject> LevelObjects = new List<LevelObject>();
     public float RecordTime { get; private set; }
 
     public void UpdateRecordTime()
     {
         var time = 0f;
-        foreach (LevelObject level in levels)
+        foreach (LevelObject level in LevelObjects)
         {
             time += level.RecordTime;
         }
