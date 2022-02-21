79f6489e534538087ba76f6f4d10227b534f0609
Export_Nico <auersw.nico@gmail.com>
2022-02-21 14:40:58 +0100

    added basic LevelTimer UI

--- Assets/UILevelTimer.cs
+++ Assets/UILevelTimer.cs
@@ -1,9 +1,20 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class UILevelTimer : MonoBehaviour
 {
+    private TextMeshProUGUI textMeshPro;
+
+    private void Start()
+    {
+        textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
+        textMeshPro.text = TimeSpan.FromSeconds(0f).ToString("mm\\:ss\\:ff");
+    }
+
     private void OnEnable()
     {
         LevelManager.onLevelTimerChanged += UpdateTimer;
@@ -17,5 +28,6 @@ public class UILevelTimer : MonoBehaviour
     private void UpdateTimer(float timer)
     {
         Debug.Log("Level Timer:_ " + timer);
+        textMeshPro.text = TimeSpan.FromSeconds(timer).ToString("mm\\:ss\\:ff");
     }
 }
