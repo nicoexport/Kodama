e002ca5532ae8a1db0c840490dae7932e21acf43
Export_Nico <auersw.nico@gmail.com>
2022-02-26 14:39:20 +0100

    Instantiating App Prefab before SceneLoad and making it DontDestroyOnLoad

---
+++ Assets/Scripts/Architecture/App.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+
+public static class App
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void InitializeApp()
+    {
+        var app = UnityEngine.Object.Instantiate(Resources.Load("App")) as GameObject;
+        if (app == null) throw new ApplicationException();
+
+        UnityEngine.Object.DontDestroyOnLoad(app);
+    }
+}
