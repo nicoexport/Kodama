70b9020d0195b445d4b8ae6127749ff8c43af396
Export_Nico <auersw.nico@gmail.com>
2022-03-01 20:22:22 +0100

    WorldManager updated into WorldsScreenManager

---
+++ Assets/Scripts/Architecture/WorldsScreenManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WorldsScreenManager : Singleton<WorldsScreenManager>
+{
+    [SerializeField]
+    private GameSessionDataSO _sessionData;
+    [SerializeField]
+    private LoadLevelEventChannelSO _loadLevelEventChannel;
+
+
+    [Header("Temporary")]
+    [SerializeField] TextMeshProUGUI worldDisplay;
+
+    private WorldDataSO _currentWorld;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _currentWorld = _sessionData.CurrentWorld;
+        UpdateWorldDisplay();
+    }
+
+
+    [ContextMenu("StartLevel")]
+    private void StartTestLevel()
+    {
+        var level = _currentWorld.LevelDatas[0];
+        _loadLevelEventChannel.RaiseEventWithLevelData(level, true, true);
+    }
+
+
+    private void UpdateWorldDisplay()
+    {
+        worldDisplay.text = _currentWorld.WorldName;
+    }
+
+    [ContextMenu("ReturnToMainMenu")]
+    private void ReturnToMainMenu()
+    {
+        GameModeManager.Instance.HandleModeStartRequested(GameModeManager.Instance.mainMenuMode);
+    }
+}
--- Assets/Scripts/Scriptables/WorldDataSO.cs
+++ Assets/Scripts/Scriptables/WorldDataSO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 [CreateAssetMenu(menuName = "Game Data/World Data")]
 public class WorldDataSO : ScriptableObject
 {
+    public string WorldName;
     public List<LevelDataSO> LevelDatas;
     public bool Visited;
     public bool Completed;
