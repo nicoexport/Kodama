e4d9a29b6896ae20de015c16c83bba18e091b985
Nico Auerswald <auersw.nico@gmail.com>
2022-04-13 17:49:58 +0200

    added audio related stuff

---
+++ Assets/Scripts/Architecture/AudioManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Architecture
+{
+    public class AudioManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Scriptable/AudioConfigSo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Scriptable
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class AudioConfigSo : ScriptableObject
+    {
+        
+    }
+}
