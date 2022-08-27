bc475b4eb5d7c63f596af10d2a886bc4111a382f
Export_Nico <auersw.nico@gmail.com>
2022-08-27 22:55:34 +0200

    update level timer

--- Assets/Scripts/UI/UILevelTimer.cs
+++ Assets/Scripts/UI/UILevelTimer.cs
@@ -12,7 +12,8 @@ namespace UI
         void Start()
         {
             textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
-            textMeshPro.text = TimeSpan.FromSeconds(0f).ToString("mm\\:ss\\:ff");
+            var span = TimeSpan.FromSeconds(0f);
+            textMeshPro.text = span.ToString("mm") + " : " + span.ToString("ss") + " : " + span.ToString("ff");
         }
 
         void OnEnable()
@@ -27,7 +28,10 @@ namespace UI
 
         void UpdateTimer(float timer)
         {
-            textMeshPro.text = TimeSpan.FromSeconds(timer).ToString("mm\\:ss\\:ff");
+            // textMeshPro.text = TimeSpan.FromSeconds(timer).ToString("mm\\:ss\\:ff");
+
+            var span = TimeSpan.FromSeconds(timer);
+            textMeshPro.text = span.ToString("mm") + " : " + span.ToString("ss") + " : " + span.ToString("ff");
         }
     }
 }
