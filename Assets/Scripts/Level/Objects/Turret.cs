8b3f13e42e3082e281185d368af4b6aa4bf83270
Nico Auerswald <auersw.nico@gmail.com>
2022-10-27 20:10:33 +0200

    implement line of sight

--- Assets/Scripts/Level/Objects/Turret.cs
+++ Assets/Scripts/Level/Objects/Turret.cs
@@ -9,21 +9,22 @@ namespace Level.Objects
 {
     public class Turret : Resettable
     {
-        
         [SerializeField] private GameObject _projectilePrefab;
         [SerializeField] private CharacterRuntimeSet _playerRuntimeSet;
         [SerializeField] private float _range;
         [SerializeField] private float _cooldownInSeconds;
         [SerializeField] private float _heatupInSeconds;
         [SerializeField] private bool _needsLineOfSight;
+        [SerializeField] private LayerMask _whatIsPlayer;
         
         private Character _player;
         private Coroutine _shootCoroutine;
         private bool _canShoot => (_player || _playerRuntimeSet.TryGetFirst(out _player))
                                   && Vector2.Distance(transform.position, _player.transform.position) <= _range;
-        
+
         public UnityEvent OnStartHeatUp;
         public UnityEvent OnShoot;
+        public UnityEvent OnCooldown;
         
         public override void OnLevelReset()
         {
@@ -37,16 +38,37 @@ namespace Level.Objects
             while (true)
             {
                 yield return new WaitUntil(()=> _canShoot);
-                Heatup();
-                yield return new WaitForSeconds(_heatupInSeconds);
-                Shoot();
-                yield return new WaitForSeconds(_cooldownInSeconds);
+                if (HasLineOfSight())
+                {
+                    Heatup();
+                    yield return new WaitForSeconds(_heatupInSeconds);
+                }
+                if (HasLineOfSight())
+                {
+                    Shoot();
+                    yield return new WaitForSeconds(_cooldownInSeconds);
+                    OnCooldown?.Invoke();
+                }
+                else
+                {
+                    OnCooldown?.Invoke();
+                }
             }
         }
 
         private void Heatup()
         {
             OnStartHeatUp.Invoke();
+        }
+
+        private bool HasLineOfSight()
+        {
+            if (!_needsLineOfSight) return true;
+            var position = transform.position;
+            var dir = -(position - _player.transform.position);
+            var res = Physics2D.Raycast(position, dir, _range);
+            Debug.Log(res.collider.CompareTag("Player"));
+            return (res.collider.CompareTag("Player"));
         }
         
         private void Shoot()
