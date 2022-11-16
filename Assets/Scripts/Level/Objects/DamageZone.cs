f0d36101810cd9a3dcd47d842a2b6378eac597e1
Nico Auerswald <auersw.nico@gmail.com>
2022-11-16 17:58:58 +0100

    test platform

--- Assets/Scripts/Level/Objects/DamageZone.cs
+++ Assets/Scripts/Level/Objects/DamageZone.cs
@@ -9,19 +9,23 @@ namespace Kodama.Level.Objects {
         protected void OnTriggerEnter2D(Collider2D col) => OnEnter(col);
 
         private void OnEnter(Collider2D col) {
-            if (col.CompareTag("Player")) {
-                var health = col.GetComponentInChildren<PlayerHealth>();
-                if (health) {
-                    health.TakeDamage(damage);
-                }
-
-                if (col.TryGetComponent(out Rigidbody2D rb)) {
-                    var force = (rb.transform.position - transform.position).normalized;
-                    force = new Vector2(force.x * damageForce.x, force.y * damageForce.y);
-                    rb.velocity = Vector2.zero;
-                    rb.AddForce(force, ForceMode2D.Impulse);
-                }
+            if (!col.CompareTag("Player")) {
+                return;
             }
+
+            var health = col.GetComponentInChildren<PlayerHealth>();
+            if (health) {
+                health.TakeDamage(damage);
+            }
+
+            if (!col.TryGetComponent(out Rigidbody2D rb)) {
+                return;
+            }
+
+            var force = (rb.transform.position - transform.position).normalized;
+            force = new Vector2(force.x * damageForce.x, force.y * damageForce.y);
+            rb.velocity = Vector2.zero;
+            rb.AddForce(force, ForceMode2D.Impulse);
         }
     }
 }
